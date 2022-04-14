581e8188dab7ef60aff7cb11626a8e22802d3f17
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-14 16:53:15 -0300

    objeto serializado json

--- src/main/java/br/com/kafka_producer/App.java
+++ src/main/java/br/com/kafka_producer/App.java
@@ -1,7 +1,11 @@
 package br.com.kafka_producer;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
+import com.google.gson.Gson;
+
+import br.com.kafka_producer.models.Aluno;
 import br.com.kafka_producer.services.KafkaService;
 
 public class App 
@@ -9,7 +13,21 @@ public class App
     public static void main( String[] args ) throws InterruptedException, ExecutionException
     {
         System.out.println("Enviando mensagem ...");
-        KafkaService.sendMessage("DANILO", "8");
+
+        var aluno = new Aluno();
+        aluno.setNome("Danilo");
+        aluno.setMatricula("001");
+
+        var notas = new ArrayList<Double>();
+        notas.add(7.0);
+        notas.add(6.0);
+        notas.add(8.0);
+
+        aluno.setNotas(notas);
+
+        String alunoParsed = new Gson().toJson(aluno);
+        
+        KafkaService.sendMessage("Aluno", alunoParsed);
     }
 }
 
