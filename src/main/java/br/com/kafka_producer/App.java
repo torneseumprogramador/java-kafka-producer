23dc3c1287ec6cc5d40163111d45f828d7ae759d
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-14 18:14:43 -0300

    mais itens para preparação da aula

--- src/main/java/br/com/kafka_producer/App.java
+++ src/main/java/br/com/kafka_producer/App.java
@@ -1,5 +1,6 @@
 package br.com.kafka_producer;
 
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import br.com.kafka_producer.services.KafkaService;
@@ -9,7 +10,8 @@ public class App
     public static void main( String[] args ) throws InterruptedException, ExecutionException
     {
         System.out.println("Enviando mensagem ...");
-        KafkaService.sendMessage("DANILO", "8");
+        // KafkaService.sendMessage("DANILO-" + UUID.randomUUID().toString(), "8");
+        KafkaService.sendMessage("DANILO-" + UUID.randomUUID().toString(), "8"); // para quando for utilizar para mais de uma partição
     }
 }
 
